33e8e6f9b254a0a02be851a736c6701260bf7f93
MarkDuenas <markgdlduenas@gmail.com>
2021-04-07 20:36:26 -0700

    working, kind of

--- src/client.js
+++ src/client.js
@@ -1,73 +1,67 @@
-'use strict';
+"use strict";
 
-const { Socket } = require('dgram');
-const readline = require('readline');
-const io = require('socket.io-client');
-const host = 'http://localhost:3333';
+const { Socket } = require("dgram");
+const readline = require("readline");
+const io = require("socket.io-client");
+const host = "http://localhost:3333";
 const tak = io.connect(host);
-
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let firstPlayer = false;
 
-tak.on('connect', () => {
-  rl.question('Enter your player name', (yourName) => {
+tak.on("connect", () => {
+  rl.question("Enter your player name: ", (yourName) => {
     console.log(`Welcome ${yourName}`);
-    // rl.close();
-    tak.emit('createGame', yourName);
+    tak.emit("createGame", yourName);
   });
-})
+});
 
-tak.on('newGame', room => {
-  console.log(`Waiting for player 2 to join ${room}`);
-})
-
-tak.on('draw', () => {
-  console.log('draw');
-})
-
-tak.on('player1', name => {
-  console.log(`Player ${name} wins!`);
-  rl.question('Want to play again? Yes or no?', response => {
-    if (response === 'yes') {
-      tak.emit('createGame', name)
+tak.on("draw", () => {
+  console.log("You both tied");
+  rl.question("Want to play again? Yes or no? ", (response) => {
+    if (response === "yes") {
+      tak.emit("restart");
     } else {
       rl.close();
     }
-  })
-})
+  });
+});
 
-tak.on('player2', name => {
+tak.on("player1", (name) => {
   console.log(`Player ${name} wins!`);
-  rl.question('Want to play again? Yes or no?', response => {
-    if (response === 'yes') {
-      tak.emit('createGame', name)
+  rl.question("Want to play again? Yes or no? ", (response) => {
+    if (response === "yes") {
+      tak.emit("restart", name);
     } else {
       rl.close();
     }
-  })
-})
+  });
+});
 
-tak.on('startgame', (player) => {
-  rl.question('Please enter rock, paper, or scissor', choice => {
-    console.log(choice);
+tak.on("player2", (name) => {
+  console.log(`Player ${name} wins!`);
+  rl.question("Want to play again? Yes or no? ", (response) => {
+    if (response === "yes") {
+      tak.emit("restart", name);
+    } else {
+      rl.close();
+    }
+  });
+});
+
+tak.on("startgame", (player) => {
+  rl.question("Please enter rock, paper, or scissor: ", (choice) => {
+    console.log(`You chose ${choice}`);
     console.log(player);
-    tak.emit('results', { choice, player });
+    tak.emit("results", { choice, player });
   });
-})
+});
 
-tak.emit('tikTak');
-tak.emit('received', messageReceived);
-
-function messageReceived() {
-  console.log('Got it');
-}
-
-rl.on('close', () => {
-  console.log('Thanks for playing, bye bye');
+rl.on("close", () => {
+  console.log("Thanks for playing, bye bye");
   process.exit(0);
-})
+});
--- src/hub.js
+++ src/hub.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 3333;
 
 const server = app.listen(port, () => {
-  console.log(`Server is up on ${port}`)
+  console.log(`Server is up on ${port}`);
 });
 
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
 let playerOne = null;
 let playerTwo = null;
@@ -17,39 +17,36 @@ let playerTwo = null;
 let choiceOne = null;
 let choiceTwo = null;
 
-
-function getWinner(result, playerOne, playerTwo) {
-  if (result === 'draw') {
-    console.log('It was a tie');
-    io.emit('draw');
+function getWinner(result, p1, p2) {
+  if (result === "draw") {
+    console.log("It was a tie");
+    io.emit("draw");
   } else if (result === true) {
-    console.log('Player One Won');
-    io.emit('player1', playerOne);
-
+    console.log("Player One Won");
+    io.emit("player1", p1);
   } else {
-    console.log('Player two won');
-    io.emit('player2', playerTwo);
+    console.log("Player two won");
+    io.emit("player2", p2);
   }
 }
-
 
 function compare(c1, c2) {
   if (c1 === c2) {
-    return 'draw';
-  } else if (c1 === 'rock') {
-    if (c2 === 'paper') {
+    return "draw";
+  } else if (c1 === "rock") {
+    if (c2 === "paper") {
       return false;
     } else {
       return true;
     }
-  } else if (c1 === 'paper') {
-    if (c2 === 'scissor') {
+  } else if (c1 === "paper") {
+    if (c2 === "scissor") {
       return false;
     } else {
       return true;
     }
-  } else if (c1 === 'scissor') {
-    if (c2 === 'rock') {
+  } else if (c1 === "scissor") {
+    if (c2 === "rock") {
       return false;
     } else {
       return true;
@@ -57,30 +54,38 @@ function compare(c1, c2) {
   }
 }
 
+io.on("connection", (socket) => {
+  console.log("Up and running");
 
-io.on('connection', (socket) => {
-  console.log('Up and running');
+  socket.on("createGame", (yourName) => {
+    console.log(`Thanks ${yourName} for joining the game!`);
+    socket.join("gameRoom");
 
-  socket.on('tikTak', (data) => {
-    console.log('Your in our room');
-  })
-
-  socket.on('createGame', yourName => {
-    socket.join('gameRoom');
     if (playerOne === null) {
       playerOne = yourName;
     } else {
       playerTwo = yourName;
     }
     if (playerOne && playerTwo) {
-      console.log('This should start the game');
-      socket.to('gameRoom').emit('startgame', playerOne);
-      socket.emit('startgame', playerTwo);
+      console.log("This is PLAYER ONE", playerOne);
+      console.log("This is PLAYER TWO", playerTwo);
+      socket.to("gameRoom").emit("startgame", playerOne);
+      socket.emit("startgame", playerTwo);
     }
-    // socket.emit('newGame', 'gameRoom');
-  })
+  });
 
-  socket.on('results', payload => {
+  socket.on("restart", (name) => {
+    if (playerOne === null) {
+      playerOne = name;
+    } else {
+      playerTwo = name;
+    }
+    socket.to("gameRoom").emit("startgame", playerOne);
+    socket.emit("startgame", playerTwo);
+  });
+
+  socket.on("results", (payload) => {
+    console.log("This is inside the results event", payload.player);
     if (playerOne === payload.player) {
       choiceOne = payload.choice;
     } else {
@@ -94,8 +99,20 @@ io.on('connection', (socket) => {
 
       getWinner(result, playerOne, playerTwo);
     }
-  })
+  });
 
-
-})
-
+  function getWinner(result, p1, p2) {
+    if (result === "draw") {
+      console.log("It was a tie");
+      socket.emit("draw");
+    } else if (result === false) {
+      console.log("Player One Won");
+      socket.to("gameRoom").emit("player1", p1);
+      socket.emit("player1", p2);
+    } else {
+      console.log("Player two won");
+      socket.emit("player2", p2);
+      socket.to("gameRoom").emit("player2", p1);
+    }
+  }
+});
